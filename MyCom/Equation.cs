14f83aa51c5c5a63e7754ee64df4c64514a64b70
wenny380 <52930464+wenny380@users.noreply.github.com>
2021-05-29 03:18:39 +0300

    Add files via upload

---
+++ MyCom/Equation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[Guid("8C034F6A-1D3F-4DB8-BC99-B73873D8C297")]
+[ClassInterface(ClassInterfaceType.None)]
+[ComVisible(true)]
+public class Equation : MyCom.IEquation
+{
+    public double LinearEq(int a, int b)
+    {
+
+        return (double)-b / a;
+    }
+
+    public string SqrEq(int a, int b, int c)
+    {
+        string result = "";
+        double D = b * b - 4 * a * c;
+        if (D > 0)
+        {
+            double sqrtD = Math.Sqrt(D);
+            double x1 = (-b + sqrtD) / 2 * a;
+            double x2 = (-b - sqrtD) / 2 * a;
+            x1 = Math.Truncate(x1 * 100) / 100;
+            x2 = Math.Truncate(x2 * 100) / 100;
+            result = "x1=" + x1.ToString() + " " + "x2=" + x2.ToString();
+            return result;
+        }
+        if (D == 0)
+        {
+            double sqrtD = Math.Sqrt(D);
+            double x1 = (-b + sqrtD) / 2 * a;
+            x1 = Math.Truncate(x1 * 100) / 100;
+            result = "x1=" + x1.ToString();
+            return result;
+        }
+        if (D < 0)
+        {
+            result = "Дискриминант меньше 0";
+            return result;
+        }
+        return result;
+    }
+}
---
+++ MyCom/IEquation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyCom
+{
+    [Guid("364C5E66-4412-48E3-8BD8-7B2BF09E8922")]
+    [ComVisible(true)]
+    public interface IEquation
+    {
+        double LinearEq(int a, int b);
+        string SqrEq(int a, int b, int c);
+
+    }
+}
